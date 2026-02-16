0321ed6ecf69a4617d5a4338a5ada6c71861afb1
NitroStacker <officialfullmana@gmail.com>
2026-02-16 15:17:08 -0500

    Player Controller
    -Added Player controller
    -Pickup, Rotate, place, throw
    -Added UI architecture
    -Added SO architecture for item desc, name, traits, etc

---
+++ InterdimensionalGroceries/Assets/Scripts/ItemSystem/PickableItem.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+using InterdimensionalGroceries.Core;
+
+namespace InterdimensionalGroceries.ItemSystem
+{
+    [RequireComponent(typeof(Rigidbody))]
+    public class PickableItem : MonoBehaviour, IPickable
+    {
+        private Rigidbody rb;
+        private Transform originalParent;
+        private int originalLayer;
+
+        private void Awake()
+        {
+            rb = GetComponent<Rigidbody>();
+        }
+
+        public void OnPickedUp()
+        {
+            originalParent = transform.parent;
+            originalLayer = gameObject.layer;
+            
+            rb.isKinematic = true;
+            rb.useGravity = false;
+        }
+
+        public void OnDropped()
+        {
+            transform.parent = originalParent;
+            gameObject.layer = originalLayer;
+            
+            rb.isKinematic = false;
+            rb.useGravity = true;
+        }
+
+        public void OnThrown(float force)
+        {
+            transform.parent = originalParent;
+            gameObject.layer = originalLayer;
+            
+            rb.isKinematic = false;
+            rb.useGravity = true;
+            rb.AddForce(Camera.main.transform.forward * force, ForceMode.Impulse);
+        }
+    }
+}
